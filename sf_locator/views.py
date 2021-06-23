2d077aa7ff32ecd9d3e8c9563d64fd7b44b40155
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-06-22 21:18:26 -0500

    saving progress on superfund site locator app

--- sf_locator/views.py
+++ sf_locator/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+# from geopy import distance
+from geopy import Nominatim
 
 # Create your views here.
 
@@ -6,10 +8,21 @@ def sf_view(request):
     return render(request, 'sf_locator.html')
 
 def sf_results_view(request):
-    street = request.GET['street'].strip()
-    city = request.GET['city'].strip()
-    state = request.GET['state'].strip()
-    zip_code = request.GET['zip_code'].strip()
+    try:
+        street = request.GET['street'].strip()
+        city = request.GET['city'].strip()
+        state = request.GET['state'].strip()
+        zip_code = request.GET['zip_code'].strip()
 
-    address = street + ", " + city + ", " + state + ", " + zip_code
-    return render(request, 'sf_locator_results.html', {'complete_address': address})
+        address = street + ", " + city + ", " + state + ", " + zip_code
+
+        geolocator = Nominatim(user_agent="my_user_agent")
+        location = geolocator.geocode(address)
+        long_address = location
+        lat = location.latitude
+        long = location.longitude
+        return render(request, 'sf_locator_results.html', {'long_address': long_address, 'latitude': lat, 
+            'longitude': long})
+        # return render(request, 'sf_locator_results.html', {"complete_address": address})
+    except:
+        return render(request, 'sf_locator.html')
