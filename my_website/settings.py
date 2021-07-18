bad3b3a7f042de24a3db845fe9d11452280ef093
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-17 21:37:16 -0500

    updated gitignore, html, settings.py

--- my_website/settings.py
+++ my_website/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,10 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",'/images/',
+    BASE_DIR / "assets",'/images/',
 ]
 
 # Default primary key field type
